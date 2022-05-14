61066f53cf4aeb46f9fab83b75317d13188bf19e
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2022-05-13 21:20:23 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+use humhub\modules\verified\Module;
+
+return [
+    'id' => 'verified',
+    'class' => Module::class,
+    'namespace' => 'humhub\modules\verified',
+    'events' => [
+    ],
+];
