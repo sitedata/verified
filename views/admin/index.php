bff9712d750cb2c8b754cda3090014096b1d7a61
Felix Hahn <info@hahn-felix.de>
2023-04-13 11:48:51 +0200

    fix width of config form
    The form was overlapping the screen.
    I have removed the <div class="container">.
    It is already present in the page (but wrapping the left menu and the form).

--- views/admin/index.php
+++ views/admin/index.php
@@ -10,25 +10,24 @@ use humhub\libs\Html;
 /* @var $this \humhub\modules\ui\view\components\View */
 ?>
 
-<div class="container">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <?= \Yii::t('VerifiedModule.base', '<strong>Verified</strong> module configuration') ?></div>
-        <div class="panel-body">
-            <?php $form = ActiveForm::begin(['id' => 'configure-form']); ?>
-            <div class="form-group">
-                <?= $form->field($model, 'verifyUser')->widget(UserPickerField::class, ['id' => 'user_id', 'maxSelection' => \Yii::$app->getModule('verified')->getMaxNumber(), 'disabledItems' => [\Yii::$app->user->guid], 'placeholder' => \Yii::t('VerifiedModule.base', 'Add a member to verify')]); ?>
-                <?= $form->field($model, 'verifySpace')->widget(SpacePickerField::class, ['id' => 'space_id', 'placeholder' => \Yii::t('VerifiedModule.base', 'Add a space to be verified.')]); ?>
-                <?= $form->field($model, 'icon')->widget(IconPicker::class, ['options' => ['placeholder' => \Yii::t('VerifiedModule.base', 'Select icon ...')]]); ?>
-                <?= $form->field($model, 'color')->widget(ColorInput::class, ['options' => ['placeholder' => \Yii::t('VerifiedModule.base', 'Select color ...')]]); ?>
-                <?= $form->field($model, 'maxNumber'); ?>
-            </div>
-
-            <div class="form-group">
-                <?= Html::submitButton(\Yii::t('VerifiedModule.base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
-            </div>
-            
-            <?php ActiveForm::end(); ?>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <?= \Yii::t('VerifiedModule.base', '<strong>Verified</strong> module configuration') ?>
+    </div>
+    <div class="panel-body">
+        <?php $form = ActiveForm::begin(['id' => 'configure-form']); ?>
+        <div class="form-group">
+            <?= $form->field($model, 'verifyUser')->widget(UserPickerField::class, ['id' => 'user_id', 'maxSelection' => \Yii::$app->getModule('verified')->getMaxNumber(), 'disabledItems' => [\Yii::$app->user->guid], 'placeholder' => \Yii::t('VerifiedModule.base', 'Add a member to verify')]); ?>
+            <?= $form->field($model, 'verifySpace')->widget(SpacePickerField::class, ['id' => 'space_id', 'placeholder' => \Yii::t('VerifiedModule.base', 'Add a space to be verified.')]); ?>
+            <?= $form->field($model, 'icon')->widget(IconPicker::class, ['options' => ['placeholder' => \Yii::t('VerifiedModule.base', 'Select icon ...')]]); ?>
+            <?= $form->field($model, 'color')->widget(ColorInput::class, ['options' => ['placeholder' => \Yii::t('VerifiedModule.base', 'Select color ...')]]); ?>
+            <?= $form->field($model, 'maxNumber'); ?>
         </div>
+
+        <div class="form-group">
+            <?= Html::submitButton(\Yii::t('VerifiedModule.base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
+        </div>
+        
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
