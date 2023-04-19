21c29ba4718797a1c452f1e9cb385d603009362f
Felix Hahn <info@hahn-felix.de>
2023-04-19 21:48:38 +0200

    Update German translation file

--- messages/de/base.php
+++ messages/de/base.php
@@ -1,6 +1,13 @@
 <?php
 
-return array(
-	'Verified User' => 'Verifizierter Benutzer',
-	'Verified Space' => 'Verifizierter Space',
-);
+return [
+    'Custom Icon color:' => '',
+    'Custom Icon used:' => '',
+    'Invalid color!' => '',
+    'Maximum number of verified allowed:' => '',
+    'Verified' => '',
+    'Verified Space' => 'Verifizierter Space',
+    'Verified User' => 'Verifizierter Benutzer',
+    'Verify Selected Space(s):' => '',
+    'Verify Selected User(s):' => '',
+];
