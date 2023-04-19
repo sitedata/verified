37c3f942996fa4d1508c3f16e017f9d31a271721
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2023-04-19 15:40:12 -0400

    Enh: Add Translation files

---
+++ messages/zh-TW/base.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Custom Icon color:' => '',
+    'Custom Icon used:' => '',
+    'Invalid color!' => '',
+    'Maximum number of verified allowed:' => '',
+    'Verified' => '',
+    'Verified Space' => '',
+    'Verified User' => '',
+    'Verify Selected Space(s):' => '',
+    'Verify Selected User(s):' => '',
+];
