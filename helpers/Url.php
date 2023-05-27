4292468827f9dc316e17c49ef117d8e6dd9eceb2
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2023-05-27 15:52:32 -0400

    Update Url.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- helpers/Url.php
+++ helpers/Url.php
@@ -3,21 +3,20 @@
 namespace humhub\modules\verified\helpers;
 
 use Yii;
-use humhub\modules\verified\models\ConfigureForm;
 use yii\helpers\Url as BaseUrl;
 
 class Url extends BaseUrl
 {
     const ROUTE_ADMIN = '/verified/admin/index';
-    const ROUTE_REQUEST = '/verified/payment/index';
+    const ROUTE_OPTIONS = '/verified/payment/index';
 
     public static function getConfigUrl()
     {
         return static::toRoute(static::ROUTE_ADMIN);
     }
 
-    public static function getRequestUrl()
+    public static function getOptionsUrl()
     {
-        return static::toRoute(static::ROUTE_REQUEST);
+        return static::toRoute(static::ROUTE_OPTIONS);
     }
 }
