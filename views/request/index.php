4f21584b0ca59b5a4abf53d874cde432ddc714fe
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2023-05-27 03:53:59 -0400

    Update index.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- views/request/index.php
+++ views/request/index.php
@@ -1,16 +1,11 @@
 <?php
 
-use humhub\modules\verified\models\RequestBadge;
 use humhub\modules\ui\form\widgets\ActiveForm;
 use humhub\widgets\ModalButton;
 use humhub\widgets\ModalDialog;
 use humhub\libs\Html;
 
-if(!isset($model)) {
-    $model = new RequestBadge();
-}
-
-/** @var $model RequestBadge */
+/** @var $model \humhub\modules\verified\models\RequestBadge */
 
 ?>
 
@@ -19,29 +14,28 @@ if(!isset($model)) {
 <div class="modal-body">
 
     <?= Html::beginTag('div') ?>
-    <script src="https://www.paypal.com/sdk/js?client-id=<?= Yii::$app->getModule('verified')->settings->get('paypalId'); ?>&enable-funding=venmo&components=buttons,funding-eligibility"></script>
-    <div id="paypal-button-container">
-    <script>
-        // Loop over each funding source
-        paypal.getFundingSources().forEach(function(fundingSource) {
-        	// Initialize the buttons
-        	var button = paypal.Buttons({
-        		fundingSource: fundingSource,
-        		  style: {
-        		      tagline: false,
-        		      layout: 'horizontal',
-        		      shape:  'rect',
-        		      label:  'paypal'
-        		  }
-        	})
-        	// Check if the button is eligible
-        	if (button.isEligible()) {
-        		// Render the standalone button for that funding source
-        		button.render('#paypal-button-container')
-        	}
-        })
+    <script src="https://www.paypal.com/sdk/js?client-id=<?= Yii::$app->getModule('verified')->settings->get('paypalId'); ?>&vault=true&intent=subscription" data-sdk-integration-source="button-factory"></script>
+    <div id="paypal-button-container"></div>
+    <script <?= Html::nonce() ?>>
+    	// Initialize the buttons
+    	paypal.Buttons({
+    		style: {
+    			tagline: false,
+    			layout: 'vertical',
+    			shape: 'rect',
+    			label: 'subscribe'
+    		},
+    		createSubscription: function(data, actions) {
+    			return actions.subscription.create({
+    				/* Creates the subscription */
+    				plan_id: '<?= Yii::$app->getModule('verified ')->settings->get('planId '); ?>'
+    			});
+    		},
+    		onApprove: function(data, actions) {
+    			alert(data.subscriptionID); // You can add optional success message for the subscriber here
+    		}
+    	}).render('#paypal-button-container');
     </script>
-    </div>
     <?= Html::endTag('div'); ?>
 </div>
 <div class="modal-footer">
