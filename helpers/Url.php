4dee76cb5b9d6d6dfb849fdf22ee2b6d0d7f1a43
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2023-05-27 15:49:21 -0400

    Update Url.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- helpers/Url.php
+++ helpers/Url.php
@@ -9,7 +9,7 @@ use yii\helpers\Url as BaseUrl;
 class Url extends BaseUrl
 {
     const ROUTE_ADMIN = '/verified/admin/index';
-    const ROUTE_REQUEST = '/verified/request/index';
+    const ROUTE_REQUEST = '/verified/payment/index';
 
     public static function getConfigUrl()
     {
