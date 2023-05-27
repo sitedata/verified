80a08d46aea67638c8e154fa6f779741f7296a1f
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2023-05-27 01:42:52 -0400

    Update Events.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- Events.php
+++ Events.php
@@ -2,11 +2,13 @@
 
 namespace humhub\modules\verified;
 
+use Yii;
 use humhub\modules\verified\widgets\VerifiedProfileHeader;
+use humhub\modules\verified\widgets\VerifiedUserWall;
 use humhub\modules\verified\widgets\VerifiedPeopleCard;
 use humhub\modules\verified\widgets\VerifiedSpaceCard;
 use humhub\modules\verified\widgets\VerifiedComment;
-
+use humhub\modules\ui\menu\MenuLink;
 use humhub\libs\WidgetCreateEvent;
 
 class Events
@@ -15,20 +17,41 @@ class Events
 	{
 		    $event->config['class'] = VerifiedProfileHeader::class;
 	}
+
 	public static function onPeopleCardBeforeRun(WidgetCreateEvent $event)
 	{
 		    $event->config['class'] = VerifiedPeopleCard::class;
 	}
+
 	public static function onSpaceDirectoryCardBeforeRun(WidgetCreateEvent $event)
 	{
 		    $event->config['class'] = VerifiedSpaceCard::class;
 	}
+
 	public static function onWallStreamEntryWidgetBeforeRun(WidgetCreateEvent $event)
 	{
 		$event->config['layoutHeader'] = '@verified/widgets/views/wallStreamEntryHeader';
 	}
+
 	public static function onCommentBeforeRun(WidgetCreateEvent $event)
 	{
 		$event->config['class'] = VerifiedComment::class;
 	}
+
+	public static function onAccountMenuInit($event)
+	{
+	    $menu = $event->sender;
+
+	    $menu->addEntry(new MenuLink([
+	        'icon' => 'fa-exclamation-circle',
+	        'label' => Yii::t('VerifiedModule.base', 'Verification Request'),
+	        'url' => '#',
+	        'htmlOptions' => [
+	            'data-action-click' => 'ui.modal.load',
+	            'data-action-click-url' => helpers\Url::getRequestUrl()
+	       ],
+	        'sortOrder' => 1000,
+            'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id === 'verified' || Yii::$app->controller->id === 'index')
+        ]));
+    }
 }
