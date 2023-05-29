fff479c2ea193bebd804dab03fa9d9f82b53b5bf
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2023-05-29 16:00:30 -0400

    Update index.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- views/admin/index.php
+++ views/admin/index.php
@@ -11,34 +11,32 @@ use humhub\libs\Html;
 
 /* @var $this \humhub\modules\ui\view\components\View */
 ?>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <?= Button::asLink(Icon::get('credit-card') . Icon::get('cog'))->link(['options'])->cssClass('pull-right btn btn-default')->tooltip(Yii::t('VerifiedModule.base', 'Payment Options')) ?>
+        <?= \Yii::t('VerifiedModule.base', '<strong>Verified</strong> module configuration') ?>
+    </div>
+    <div class="panel-body">
+        <?php $form = ActiveForm::begin(['id' => 'configure-form']); ?>
 
-<div class="container">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <?= Button::asLink(Icon::get('credit-card') . Icon::get('cog'))->link(['options'])->cssClass('pull-right btn btn-default')->tooltip(Yii::t('AdminModule.base', 'Payment Options')) ?>
-            <?= \Yii::t('VerifiedModule.base', '<strong>Verified</strong> module configuration') ?>
-        </div>
-        <div class="panel-body">
-            <?php $form = ActiveForm::begin(['id' => 'configure-form']); ?>
-                <div class="form-group">
-                    <?= $form->field($model, 'verifyUser')->widget(UserPickerField::class, ['id' => 'user_id', 'maxSelection' => \Yii::$app->getModule('verified')->getMaxUsers()]); ?>
-                    <?= $form->field($model, 'maxUsers'); ?>
-                </div>
+            <div class="form-group">
+                <?= $form->field($model, 'verifyUser')->widget(UserPickerField::class, ['id' => 'user_id', 'maxSelection' => \Yii::$app->getModule('verified')->getMaxUsers()]); ?>
+                <?= $form->field($model, 'maxUsers'); ?>
+            </div>
 
-                <div class"form-group">
-                    <?= $form->field($model, 'verifySpace')->widget(SpacePickerField::class, ['id' => 'space_id', 'maxSelection' => \Yii::$app->getModule('verified')->getMaxSpaces()]); ?>
-                    <?= $form->field($model, 'maxSpaces'); ?>
-                </div>
+            <div class"form-group">
+                <?= $form->field($model, 'verifySpace')->widget(SpacePickerField::class, ['id' => 'space_id', 'maxSelection' => \Yii::$app->getModule('verified')->getMaxSpaces()]); ?>
+                <?= $form->field($model, 'maxSpaces'); ?>
+            </div>
 
-                <div class"form-group">
-                    <?= $form->field($model, 'icon')->widget(IconPicker::class); ?>
-                    <?= $form->field($model, 'color')->widget(ColorInput::class); ?>
-                </div>
+            <div class"form-group">
+                <?= $form->field($model, 'icon')->widget(IconPicker::class); ?>
+                <?= $form->field($model, 'color')->widget(ColorInput::class); ?>
+            </div>
 
-                <div class="form-group">
-                    <?= Html::submitButton(\Yii::t('base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
-                </div>
-            <?php ActiveForm::end(); ?>
-        </div>
+            <div class="form-group">
+                <?= Html::submitButton(\Yii::t('VerifiedModule.base', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => '']); ?>
+            </div>
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
