963a3bcd1908d4238539867adbaf32de9484bb1f
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2023-05-31 07:32:19 -0400

    Fix: Loading by disabling pjax in ActiveForm
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- views/payment/index.php
+++ views/payment/index.php
@@ -10,32 +10,39 @@ use humhub\libs\Html;
 ?>
 
 <?php ModalDialog::begin(['header' => Yii::t('VerifiedModule.base', '<strong>Verification</strong> Payment Options')]); ?>
-<?php $form = ActiveForm::begin();?>
+<?php $form = ActiveForm::begin(['options' => ['data-pjax' => 0]]); ?> 
 <div class="modal-body">
     <?= Yii::t('VerifiedModule.base', 'Welcome to ' . Yii::$app->name . ' verification center, once we confirm the payment you will receive a notification stating that your account has been verified.') ?>
     <hr>
     <?= Html::beginTag('div') ?>
-    <script <?= Html::nonce() ?> src="https://www.paypal.com/sdk/js?client-id=<?= Yii::$app->getModule('verified')->settings->get('paypalId'); ?>&vault=true&intent=subscription" data-sdk-integration-source="button-factory"></script>
     <div id="paypal-button-container"></div>
     <script <?= Html::nonce() ?>>
-    	// Initialize the buttons
-    	paypal.Buttons({
-    		style: {
-    			tagline: false,
-    			layout: 'vertical',
-    			shape: 'rect',
-    			label: 'subscribe'
-    		},
-    		createSubscription: function(data, actions) {
-    			return actions.subscription.create({
-    				/* Creates the subscription */
-    				plan_id: '<?= Yii::$app->getModule('verified')->settings->get('planId'); ?>'
-    			});
-    		},
-    		onApprove: function(data, actions) {
-    			alert(data.subscriptionID); // You can add optional success message for the subscriber here
-    		}
-    	}).render('#paypal-button-container');
+    function loadAsync(url, callback) {
+    	var s = document.createElement('script');
+    	s.setAttribute('src', url);
+    	s.onload = callback;
+    	document.head.insertBefore(s, document.head.firstElementChild);
+    }
+
+    loadAsync('https://www.paypal.com/sdk/js?client-id=<?= Yii::$app->getModule('verified')->settings->get('paypalId');?>&vault=true&intent=subscription', function() {
+    		paypal.Buttons({
+    			style: {
+    				tagline: false,
+    				layout: 'vertical',
+    				shape: 'rect',
+    				label: 'subscribe'
+    			},
+    			createSubscription: function(data, actions) {
+    				return actions.subscription.create({
+    					/* Creates the subscription */
+    					plan_id: '<?= Yii::$app->getModule('verified')->settings->get('planId'); ?>'
+    				});
+    			},
+    			onApprove: function(data, actions) {
+    				alert(data.subscriptionID); // You can add optional success message for the subscriber here
+    			}
+    		}).render('#paypal-button-container');
+    	});
     </script>
     <?= Html::endTag('div'); ?>
 </div>
